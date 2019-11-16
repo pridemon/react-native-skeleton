f25b8892fcf8327c7f595ceb36a051a4ec246768
Kasl <roman.muratov@gmail.com>
2019-11-16 15:47:50 +0700

    initial

---
+++ babel.config.js
@@ -0,0 +1,12 @@
+module.exports = (api) => {
+  const productionPlugins = api.env('production') ? [["transform-remove-console"]] : []
+
+  return {
+    presets: [
+      'module:metro-react-native-babel-preset'
+    ],
+    plugins: [
+      ...productionPlugins,
+    ]
+  }
+}
