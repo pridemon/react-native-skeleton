25cc97ab638f1774603304bb2b6da87fdedc3015
Kasl <roman.muratov@gmail.com>
2019-11-21 16:05:30 +0700

    add eslint-plugin-sonarjs

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,11 @@
 module.exports = {
   root: true,
-  extends: '@react-native-community',
+  extends: ['@react-native-community', 'plugin:sonarjs/recommended'],
+  plugins: ['sonarjs'],
+  rules: {
+    'sonarjs/cognitive-complexity': 0,
+    'sonarjs/no-duplicate-string': 1,
+    'sonarjs/no-identical-functions': 1,
+    'sonarjs/no-collapsible-if': 1,
+  },
 };
