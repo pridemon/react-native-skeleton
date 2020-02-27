2db801f052763e8c67eab80b18fd93df2bd7e31d
Kasl <roman.muratov@gmail.com>
2020-02-27 14:48:05 +0700

    run lint:fix

--- __tests__/App-test.tsx
+++ __tests__/App-test.tsx
@@ -3,10 +3,10 @@
  */
 
 import 'react-native';
-import React from 'react';
+//import React from 'react';
 
 // Note: test renderer must be required after react-native.
-import renderer from 'react-test-renderer';
+//import renderer from 'react-test-renderer';
 
 it('renders correctly', () => {
   // write first test here
--- babel.config.js
+++ babel.config.js
@@ -1,12 +1,8 @@
-module.exports = (api) => {
-  const productionPlugins = api.env('production') ? [["transform-remove-console"]] : []
+module.exports = api => {
+  const productionPlugins = api.env('production') ? [['transform-remove-console']] : [];
 
   return {
-    presets: [
-      'module:metro-react-native-babel-preset'
-    ],
-    plugins: [
-      ...productionPlugins,
-    ]
-  }
-}
+    presets: ['module:metro-react-native-babel-preset'],
+    plugins: [...productionPlugins],
+  };
+};
