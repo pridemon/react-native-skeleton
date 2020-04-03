cea8309bf36376e0e37c81c12b78eb81a442bd2a
 Vadim <VadimandSanek@yandex.ru>
2020-04-03 11:14:09 +0700

    testing patch

--- merge.sh
+++ merge.sh
@@ -4,17 +4,23 @@ mergeDestination="$1"
 [[ -z "$1" ]] && { echo "Parameter destination path is empty" ; exit 1; }
 
 skeletonDir=$(cd `dirname $0` && pwd)
+tag="0.0.1"
+patch="patch.patch"
 mergeBranch="merge_branch"
 
-git checkout -b $mergeBranch
+git checkout -b $mergeBranch $tag
 
-read -p 'Project name ' projectName
-read -p 'Android bundle identifier, e.g. com.wachanga.* ' androidBundle
+# read -p 'Project name ' projectName
+# read -p 'Android bundle identifier, e.g. com.wachanga.* ' androidBundle
 
 # react-native-rename $projectName -b $androidBundle
 
-git add git diff 0.0.1 origin/test_branch
-git commit -m "rename project"
+git diff $tag origin/test_branch > $patch
+git apply $patch
+git add .
+git rm $patch merge.sh —cached
+#git add git diff 0.0.1 origin/test_branch
+git commit -m "merge skeleton"
 
 cd "$mergeDestination" || exit
 git remote add skeleton "$skeletonDir"
