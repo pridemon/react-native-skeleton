8a6ba233f9888740b36fa6e576a11d449a6b1dae
Kasl <roman.muratov@gmail.com>
2020-04-09 21:05:43 +0700

    update eslint rules. Add typescript module

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,11 +1,23 @@
 module.exports = {
   root: true,
   extends: ['@react-native-community', 'plugin:sonarjs/recommended'],
-  plugins: ['sonarjs'],
+  parser: '@typescript-eslint/parser',
+  plugins: ['sonarjs', '@typescript-eslint'],
   rules: {
     'sonarjs/cognitive-complexity': 0,
     'sonarjs/no-duplicate-string': 1,
     'sonarjs/no-identical-functions': 1,
     'sonarjs/no-collapsible-if': 1,
+    'prefer-const': 2,
+    '@typescript-eslint/indent': "off",
+    '@typescript-eslint/camelcase': 0,
+    '@typescript-eslint/explicit-function-return-type': [
+      'error',
+      {
+        allowExpressions: false,
+        allowTypedFunctionExpressions: true,
+        allowHigherOrderFunctions: false,
+      },
+    ],
   },
 };
